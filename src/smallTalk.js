b7dd578d7d3f21edb10c7ea7609c2a002cf35484
Anthony DeVarti <98314025+anthony-devarti@users.noreply.github.com>
2025-11-23 08:47:49 +0000

    add hints to small talk array

--- src/smallTalk.js
+++ src/smallTalk.js
@@ -4,14 +4,19 @@
 const SMALL_TALK_TOPICS = [
     "the weather in Farhearth",
     "a new bench someone built near the well",
-    "a traveling bard who passed through recently",
+    "a traveling bard passed through recently, and most residents who heard him found themselves robbed clean the next day.  He's move on, since.", //story seed
     "the sound of the river last night",
     "fresh bricks being added to the town wall",
-    "a stray cat that’s been napping on the windowsill",
+    "a stray cat that's been napping on the windowsill",
     "the lanterns being cleaned for the evening",
+    "The Chronicle, the historical archives, is in desperate need of information from beyond the walls of Farhearth",
     "someone repairing their boots outside the inn",
     "the smell of bread drifting from the bakery",
-    "the quiet calm around the central square"
+    "the quiet calm around the central square",
+    "a favorite chair in the tavern, very easy to fall asleep in",
+    "most of the wares at the Curiosity Shoppe don't seem very useful.  How does it stay in business?", //hint that the Curiosity Shoppe items may be relevant later.
+    "supposedly there is a magic item that allows you to return to town safely, but it doesn't let you leave anyone behind", //Hearthstone hint from curiosity shoppe
+    "A recent trade caravan was intercepted by a pack wolves, who allegedly dug a hidden trench in the road to damage the wagon wheels to prevent escape" //tip about upcoming adventure with wolves in Eastlight Upon the Sea
 ]
 
 module.exports = {SMALL_TALK_TOPICS}
